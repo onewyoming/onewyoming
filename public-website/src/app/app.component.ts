6960f1cbb899eb094104924b3df16ac2bc64b417
Kushal <kushaldeveloper@gmail.com>
2018-08-22 17:35:32 -0400

    add article update

--- public-website/src/app/app.component.ts
+++ public-website/src/app/app.component.ts
@@ -9,3 +9,11 @@ export class AppComponent {
   title = 'wyoming';
   subTitle = 'the state of equality';
 }
+
+// document.body.innerHTML = greeter(user);
+const a = function() {
+  setInterval(function() {
+    document.getElementById('apple').innerText = new Date().toString();
+  }, 1000);
+  };
+a();
