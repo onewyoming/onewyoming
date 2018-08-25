b8d55f773c940cc83e5b12a000d3095205f9c365
Kushal Hada <kushaldeveloper@gmail.com>
2018-08-25 11:04:19 -0400

    try to make project deployable

---
+++ development.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash 
+. ~/.nvm/nvm.sh
+nvm --version;
+free -h;
+nvm install node;
+node --version;
+npm --version;
+cd /root/wyoming;
+docker-compose down --remove-orphans;
+git clean -df 
+git reset --hard;
+git pull origin master;
+cd /root/wyoming/public-website;
+rm -rf node_modules;
+yarn install;
+yarn run generate;
+free -h;
+rsync -av /root/wyoming/public-website/public/ /root/wyoming/built-website/;
+cat /root/wyoming/built-website/index.html;
+du -sh /root/wyoming/built-website;
+ls -a /root/wyoming/built-website;
+cd /root/wyoming/;docker-compose up --build -d;
+free -h;
+w;
+last;
+uptime;
