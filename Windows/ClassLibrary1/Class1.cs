090b47dc8560a8e39941d7ebdc18133b960cd43e
kushal <kushaldeveloper@gmail.com>
2018-08-25 19:56:41 -0400

    add wpf application, dot net core library, and unit test

---
+++ Windows/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Guid guid { get; set; }
+        public string name { get; set; }
+        public Class1(string name)
+        {
+            this.name = name;
+        }
+    }
+}
