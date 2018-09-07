89c187b7b46080aa1a4912aa48f81b7bb980960c
Kushal Hada <kushaldeveloper@gmail.com>
2018-09-07 19:03:39 -0400

    create a database script

---
+++ public-databases/create-tables.sql
@@ -0,0 +1,16 @@
+create schema wyoming;
+
+CREATE TABLE wyoming.person(
+id SERIAL PRIMARY KEY,
+profile_picture BYTEA,
+facebook_id INTEGER,
+email VARCHAR(400),
+password VARCHAR(60) NOT NULL,
+password_salt VARCHAR(300),
+password_hash_algorithm VARCHAR(300),
+registration_time TIMESTAMP,
+email_confirmation_token VARCHAR(100),
+password_reminder_token VARCHAR(100),
+password_reminder_expiration TIMESTAMP,
+status INTEGER 
+);
