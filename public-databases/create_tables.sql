9cdcbd695f8825b9ee537f4c775f260631489f57
Kushal Hada <kushaldeveloper@gmail.com>
2018-09-07 19:48:04 -0400

    create table, insert data, query data

--- public-databases/create_tables.sql
+++ public-databases/create_tables.sql
@@ -2,14 +2,7 @@ create schema wyoming;
 
 CREATE TABLE wyoming.person(
 id SERIAL PRIMARY KEY,
-profile_picture BYTEA,
 email VARCHAR(400),
-password VARCHAR(60) NOT NULL,
-password_salt VARCHAR(300),
-password_hash_algorithm VARCHAR(300),
-registration_time TIMESTAMP,
-email_confirmation_token VARCHAR(100),
-password_reminder_token VARCHAR(100),
-password_reminder_expiration TIMESTAMP,
-status INTEGER 
+password VARCHAR(120) NOT NULL,
+registration_time TIMESTAMP
 );
