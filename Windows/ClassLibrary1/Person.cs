6c964c0ab47e01f6d352f79fd56bb35ad604bf7d
kushal <kushaldeveloper@gmail.com>
2018-09-07 12:48:08 -0400

    add a new person class

---
+++ Windows/ClassLibrary1/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Person
+    {
+        public System.Guid Identifier { get; private set; }
+        public Person()
+        {
+            Identifier = System.Guid.NewGuid();
+        }
+    }
+}
