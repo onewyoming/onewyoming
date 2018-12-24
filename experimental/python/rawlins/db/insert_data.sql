49c0b1aacf8906522fdb91c6b2ea4f36f00a2ace
Kushal <kushaldeveloper@gmail.com>
2018-12-24 16:46:34 -0500

    add column names

--- experimental/python/rawlins/db/insert_data.sql
+++ experimental/python/rawlins/db/insert_data.sql
@@ -1,4 +1,4 @@
-insert into users values (
+insert into users (id, email, password, registration_time) values (
     1,
     "hikingfan@gmail.com",
     "pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636",
