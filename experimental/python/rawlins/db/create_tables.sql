b1789a3c4cd46cb94227fcebe3b11f591d7b59ca
Kushal <kushaldeveloper@gmail.com>
2018-12-24 16:39:32 -0500

    create insert data table

--- experimental/python/rawlins/db/create_tables.sql
+++ experimental/python/rawlins/db/create_tables.sql
@@ -1,14 +1,6 @@
--- drop database if exists rawlins;
--- drop role if exists rawlins;
--- create user rawlins createdb password 'rawlins';
--- create database rawlins owner rawlins;
--- \connect rawlins;
--- drop schema if exists rawlins;
--- create schema rawlins;
--- drop table if exists rawlins.users;
-CREATE TABLE users(
-id SERIAL PRIMARY KEY,
-email VARCHAR(400),
-password VARCHAR(120) NOT NULL,
-registration_time TIMESTAMP
+create table users (
+id serial primary key,
+email varchar(400) not null unique,
+password varchar(120),
+registration_time timestamp
 );
---
+++ experimental/python/rawlins/db/insert_data.sql
@@ -0,0 +1,7 @@
+insert into users values (
+    1,
+    "hikingfan@gmail.com",
+    "pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636",
+    current_timestamp,
+    1
+);
