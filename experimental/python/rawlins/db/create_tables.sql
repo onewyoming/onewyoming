ccc671623766bb3c2b03fb18fce1fd2af01f8bff
Kushal <kushaldeveloper@gmail.com>
2018-12-25 07:24:44 -0500

    add missing comma

--- experimental/python/rawlins/db/create_tables.sql
+++ experimental/python/rawlins/db/create_tables.sql
@@ -2,6 +2,6 @@ create table users (
 id uuid DEFAULT uuid_generate_v4 (),
 email varchar(400) not null unique,
 password varchar(120),
-registration_time timestamp
+registration_time timestamp,
 PRIMARY KEY (id)
 );
