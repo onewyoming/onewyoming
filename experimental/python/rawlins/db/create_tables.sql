c3f5471eeff9debb35eb70219393fac76168d05f
Kushal <kushaldeveloper@gmail.com>
2018-12-26 11:08:43 -0500

    add a new table room group
    /home/kus/src/personal/wyoming.gitlab.io/experimental/python/rawlins/db/create_tables.sql
    Warning:(2, 8) Unknown database function 'uuid_generate_v4'
    Warning:(6, 34) Unknown database function 'uuid_generate_v4'
    Warning:(15, 33) Unknown database function 'uuid_generate_v4'

--- experimental/python/rawlins/db/create_tables.sql
+++ experimental/python/rawlins/db/create_tables.sql
@@ -3,9 +3,19 @@ SELECT uuid_generate_v4();
 
 create table if not exists users
 (
-id uuid DEFAULT uuid_generate_v4 (),
-email varchar(400) not null unique,
-password varchar(120),
-registration_time timestamp,
-PRIMARY KEY (id)
+  id                uuid default uuid_generate_v4(),
+  email             varchar(400) not null unique,
+  password          varchar(120),
+  registration_time timestamp,
+  primary key (id)
 );
+
+create table if not exists roomgroup
+(
+  id               uuid default uuid_generate_v4(),
+  name             varchar(40) not null unique,
+  minimum_capacity int         not null,
+  maximum_capacity int         not null,
+  note             text,
+  primary key (id)
+)
