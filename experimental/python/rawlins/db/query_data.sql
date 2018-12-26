60c1d5f3e29a6c68b6f3372f0bb83d52d3b32f7e
Kushal <kushaldeveloper@gmail.com>
2018-12-26 13:54:13 -0500

    show time zone

--- experimental/python/rawlins/db/query_data.sql
+++ experimental/python/rawlins/db/query_data.sql
@@ -1,7 +1,9 @@
-select id, email, password, registration_time from users;
+select id, email, password, registration_time
+from users;
 select id, name, minimum_capacity, maximum_capacity, note
 from room_groups;
 select id, name, minimum_capacity, maximum_capacity, note, room_group
 from rooms;
 select id, start_time, end_time, room_id, user_id
 from reservations;
+show timezone;
