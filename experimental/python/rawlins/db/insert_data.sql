80b35bbc3e8b640e226cd813af704cd9af11f164
Kushal <kushaldeveloper@gmail.com>
2018-12-26 13:16:13 -0500

    insert data into room groups

--- experimental/python/rawlins/db/insert_data.sql
+++ experimental/python/rawlins/db/insert_data.sql
@@ -1,5 +1,10 @@
-insert into users (email, password, registration_time) values (
-    'hikingfan@gmail.com',
-    'pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636',
-    current_timestamp
-);
+insert into users (email, password, registration_time)
+values ('hikingfan@gmail.com',
+        'pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636',
+        current_timestamp);
+
+insert into room_groups (name, minimum_capacity, maximum_capapcity, note)
+values ('Sania Mirza Conference Suites',
+        4,
+        48,
+        'Sania Mirza conference rooms are usually for small groups but we can accommodate up to four dozen people');
