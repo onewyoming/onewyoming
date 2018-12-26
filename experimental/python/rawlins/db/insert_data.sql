0eb98c083874aeb48943ff313e8608ad02726a9b
Kushal <kushaldeveloper@gmail.com>
2018-12-26 13:28:01 -0500

    spell capacity correctly

--- experimental/python/rawlins/db/insert_data.sql
+++ experimental/python/rawlins/db/insert_data.sql
@@ -3,7 +3,7 @@ values ('hikingfan@gmail.com',
         'pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636',
         current_timestamp);
 
-insert into room_groups (name, minimum_capacity, maximum_capapcity, note)
+insert into room_groups (name, minimum_capacity, maximum_capacity, note)
 values ('Sania Mirza Conference Suites',
         4,
         48,
