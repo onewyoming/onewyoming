1c52f2639fa4ec5988578bf6495c14bdd37b5998
Kushal <kushaldeveloper@gmail.com>
2018-12-27 08:05:04 -0500

    add alter database time zone to utc
    /home/kus/src/personal/wyoming.gitlab.io/experimental/python/rawlins/db/insert_data.sql
    Warning:(8, 13) Unable to resolve table 'room_groups'
    Warning:(8, 26) Unable to resolve column 'name'
    Warning:(8, 32) Unable to resolve column 'minimum_capacity'
    Warning:(8, 50) Unable to resolve column 'maximum_capacity'
    Warning:(8, 68) Unable to resolve column 'note'
    Warning:(14, 13) Unable to resolve table 'rooms'
    Warning:(14, 20) Unable to resolve column 'name'
    Warning:(14, 26) Unable to resolve column 'minimum_capacity'
    Warning:(14, 44) Unable to resolve column 'maximum_capacity'
    Warning:(14, 62) Unable to resolve column 'note'
    Warning:(14, 68) Unable to resolve column 'room_group'
    Warning:(19, 8) Unable to resolve symbol 'room_groups'
    Warning:(20, 6) Unable to resolve table 'room_groups'
    Warning:(21, 7) Unable to resolve column 'name'
    Warning:(24, 13) Unable to resolve table 'reservations'
    Warning:(24, 27) Unable to resolve column 'start_time'
    Warning:(24, 39) Unable to resolve column 'end_time'
    Warning:(24, 49) Unable to resolve column 'room_id'
    Warning:(24, 58) Unable to resolve column 'user_id'
    Warning:(27, 8) Unable to resolve symbol 'rooms'
    Warning:(29, 6) Unable to resolve table 'rooms'
    Warning:(31, 7) Unable to resolve symbol 'rooms'

--- experimental/python/rawlins/db/insert_data.sql
+++ experimental/python/rawlins/db/insert_data.sql
@@ -1,3 +1,5 @@
+ALTER DATABASE rawlins SET timezone TO 'UTC';
+
 insert into users (email, password, registration_time)
 values ('hikingfan@gmail.com',
         'pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636',
