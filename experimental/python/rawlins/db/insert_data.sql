3db15ea7fbdb402b0ef4f8ff1faeba80bba4b0dd
Kushal <kushaldeveloper@gmail.com>
2018-12-28 11:14:13 -0500

    remove users dot

--- experimental/python/rawlins/db/insert_data.sql
+++ experimental/python/rawlins/db/insert_data.sql
@@ -5,7 +5,7 @@ values ('hikingfan@gmail.com',
         'pbkdf2:sha256:50000$SDkLJtq2$21296d9762a82c5ee6a49575d3a8bd66788468b788dcddd66dc38efe7dbdf636',
         current_timestamp, true, true, false);
 
-insert into user_tokens(select id from users where users.email = 'hikingfan@gmail.com');
+insert into user_tokens(select id from users where email = 'hikingfan@gmail.com');
 
 insert into room_groups (name, minimum_capacity, maximum_capacity, note)
 values ('Sania Mirza Conference Suites',
