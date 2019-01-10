074e4acb83f15edb9a7d2d1624dac6977a57f93d
Kushal <kushaldeveloper@gmail.com>
2019-01-10 14:20:39 -0500

    use explicit private

--- experimental/dotnet/ConsoleApp1/ConsoleApp1/Program.cs
+++ experimental/dotnet/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,9 +4,9 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Fedora!");
         }
     }
 }
