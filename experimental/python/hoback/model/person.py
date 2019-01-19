6dea7bef8473a2a0692b89d11e616dd5bc8c9c56
Kushal <kushaldeveloper@gmail.com>
2019-01-19 09:07:45 -0500

    add a primary email address to a person
    hello, YouTube!

--- experimental/python/hoback/model/person.py
+++ experimental/python/hoback/model/person.py
@@ -1,11 +1,14 @@
 import attr
 import pendulum
 
+from model.email_address import EmailAddress
+
 
 @attr.s(auto_attribs=True)
 class Person(object):
     name: str
     date_of_birth: pendulum.DateTime
+    primary_email: EmailAddress
 
     def age(self, current_date: pendulum.DateTime) -> pendulum:
         return current_date - self.date_of_birth
--- experimental/python/hoback/tests/test_person.py
+++ experimental/python/hoback/tests/test_person.py
@@ -1,6 +1,7 @@
 import pendulum
 import pytest
 
+from model.email_address import EmailAddress
 from model.person import Person
 
 
@@ -8,9 +9,16 @@ from model.person import Person
 def create_person():
     name: str = "Sarah Michelle Gellar"
     date_of_birth: pendulum.DateTime = pendulum.datetime(1977, 4, 14, 8, 8, 0)
-    person = Person(name=name, date_of_birth=date_of_birth)
+    local_part: str = "hikingfan"
+    domain: str = "gmail.com"
+    primary_email: EmailAddress = EmailAddress(local_part=local_part, domain=domain)
+    person = Person(name=name, date_of_birth=date_of_birth, primary_email=primary_email)
     return person
 
 
 def test_age(create_person):
     assert create_person.age(pendulum.now()).days > 15254
+
+
+def test_email(create_person):
+    assert "hikingfan@gmail.com" == str(create_person.primary_email)
