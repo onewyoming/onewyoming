3c588c56aa9e621ea4ca412b0281d7af25330191
Kushal <kushaldeveloper@gmail.com>
2019-01-19 00:53:48 -0500

    use pendulum and attrs

---
+++ experimental/python/hoback/model/person.py
@@ -0,0 +1,11 @@
+import attr
+import pendulum
+
+
+@attr.s
+class Person(object):
+    name: str = attr.ib()
+    date_of_birth: pendulum.datetime = attr.ib()
+
+    def age(self):
+        return pendulum.now() - self.date_of_birth
