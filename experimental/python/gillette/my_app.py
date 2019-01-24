6331c70ad7dc66e0f7e54e214bf5e14d3ad43609
Kushal <kushaldeveloper@gmail.com>
2019-01-24 12:40:25 -0500

    delete gillette
    don't need it

--- experimental/python/gillette/my_app.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-from settings import aws_access_key_id
-
-session = boto3.Session(profile_name='glacier')
-glacier = boto3.resource('glacier')
-account = glacier.Account(aws_access_key_id)
-for vault in account.vaults.all():
-    print(vault)
