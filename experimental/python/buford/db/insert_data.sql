0bf080b9a9be67f153cf0718608f2c57c45160f5
Kushal <kushaldeveloper@gmail.com>
2019-02-17 08:17:41 -0500

    applicant should be applicants

--- experimental/python/buford/db/insert_data.sql
+++ experimental/python/buford/db/insert_data.sql
@@ -1,3 +1,3 @@
 -- insert into applicant (email, registration_time) values ('hikingfan@gmail.com', now());
-insert into applicant (email, registration_time)
+insert into applicants (email, registration_time)
 values ('hikingfan@gmail.com', '2018-12-31 12:06:36.856463+00:00');
