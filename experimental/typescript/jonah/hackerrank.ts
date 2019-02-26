51bff3dd75da6b739147cad114a70fba16057344
Kushal <kushaldeveloper@gmail.com>
2019-02-25 20:41:12 -0500

    add hacker rank file
    yarn update typescript

---
+++ experimental/typescript/jonah/hackerrank.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+main();
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s: string) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let myConsonants = [];
+    for (var i = 0; i < s.length; i++) {
+        // console.log(s[i]);
+        if (vowels.includes(s[i])) {
+            console.log(s[i]);
+        }
+        else {
+            myConsonants.push(s[i]);
+        }
+    };
+    myConsonants.forEach(function (element) {
+        console.log(element);
+    });
+}
+
+function main() {
+    const s = "javascriptloops";
+    vowelsAndConsonants(s);
+}
