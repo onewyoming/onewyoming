53f4f81510b230cc638107af8a7b8ad17593208f
Kushal <kushaldeveloper@gmail.com>
2019-03-31 18:39:35 -0400

    add new project for hacker rank and leet code

---
+++ experimental/python/goshen/sock-merchant/sock-merchant.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import os
+
+
+# Complete the sockMerchant function below.
+def sock_merchant(n, ar):
+    return 3
+
+
+if __name__ == '__main__':
+    n = 9
+    ar = {10, 20, 20, 10, 10, 30, 50, 10, 20}
+    result = sock_merchant(n, ar)
+    assert result == 3
