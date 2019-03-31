af0944337d9b1842b0dcb9d1a02d3590071d1bf6
Kushal <kushaldeveloper@gmail.com>
2019-03-31 19:03:06 -0400

    add tests
    but they fail why?
    
    SockMerchant.sock_merchant import get_number_of_matching_pairs_of_socks
    E   ModuleNotFoundError: No module named 'SockMerchant'
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Interrupted: 1 errors during collection !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    ========================================================================================================================= 1 error in 0.07 seconds ==========================================================================================================================
    (goshen) [kus@localhost goshen]$ pytest
    =========================================================================================================================== test session starts ============================================================================================================================
    platform linux -- Python 3.7.2, pytest-4.4.0, py-1.8.0, pluggy-0.9.0
    rootdir: /home/kus/src/wyoming/experimental/python/goshen
    collected 0 items / 1 errors
    
    ================================================================================================================================== ERRORS ==================================================================================================================================
    _______________________________________________________________________________________________________________ ERROR collecting tests/test_sock_merchant.py _______________________________________________________________________________________________________________
    ImportError while importing test module '/home/kus/src/wyoming/experimental/python/goshen/tests/test_sock_merchant.py'.
    Hint: make sure your test modules/packages have valid Python names.
    Traceback:
    tests/test_sock_merchant.py:1: in <module>
        from sock_merchant import get_number_of_matching_pairs_of_socks
    E   ModuleNotFoundError: No module named 'sock_merchant'
    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Interrupted: 1 errors during collection !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    ========================================================================================================================= 1 error in 0.08 seconds ==========================================================================================================================
    (goshen) [kus@localhost goshen]$

---
+++ experimental/python/goshen/tests/test_sock_merchant.py
@@ -0,0 +1,8 @@
+from sock_merchant import get_number_of_matching_pairs_of_socks
+
+
+def test_get_number_of_matching_pairs_of_socks():
+    n = 9
+    ar = {10, 20, 20, 10, 10, 30, 50, 10, 20}
+    result = get_number_of_matching_pairs_of_socks(n, ar)
+    assert result == 3
