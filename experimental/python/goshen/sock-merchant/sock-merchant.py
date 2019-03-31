cddd66deac299dad33b255eccda61a9beee15011
Kushal <kushaldeveloper@gmail.com>
2019-03-31 18:48:35 -0400

    add docs
    https://stackoverflow.com/questions/2357230/what-is-the-proper-way-to-comment-functions-in-python
    
    https://www.hackerrank.com/challenges/sock-merchant/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup

--- experimental/python/goshen/sock-merchant/sock-merchant.py
+++ experimental/python/goshen/sock-merchant/sock-merchant.py
@@ -1,15 +1,19 @@
 #!/bin/python3
 
-import os
 
-
-# Complete the sockMerchant function below.
-def sock_merchant(n, ar):
+def get_number_of_matching_pairs_of_socks(number_of_socks_in_pile, sock_colors):
+    """
+    Given an array of integers representing the color of each sock,
+    determine how many pairs of socks with matching colors there are.
+    :param number_of_socks_in_pile: the number of socks in the pile
+    :param sock_colors: an array representing the colors of each sock
+    :return: the number of matching pairs of socks that are available
+    """
     return 3
 
 
 if __name__ == '__main__':
     n = 9
     ar = {10, 20, 20, 10, 10, 30, 50, 10, 20}
-    result = sock_merchant(n, ar)
+    result = get_number_of_matching_pairs_of_socks(n, ar)
     assert result == 3
