68d8d6b1fd4f03d9311d4e289d9b530040e96ef1
Kushal <kushaldeveloper@gmail.com>
2019-03-31 20:35:30 -0400

    fix

--- experimental/python/goshen/sock_merchant.py
+++ experimental/python/goshen/sock_merchant.py
@@ -9,5 +9,10 @@ def get_number_of_matching_pairs_of_socks(number_of_socks_in_pile, sock_colors):
     :param sock_colors: an array representing the colors of each sock
     :return: the number of matching pairs of socks that are available
     """
-    print(len(sock_colors))
-    return 3
+    from collections import Counter
+    sock_colors_dictionary = Counter(sock_colors)
+    my_count = 0
+    for sock_count in sock_colors_dictionary.values():
+        current_counter = sock_count // 2
+        my_count = my_count + current_counter
+    return my_count
--- experimental/python/goshen/tests/test_sock_merchant.py
+++ experimental/python/goshen/tests/test_sock_merchant.py
@@ -6,3 +6,10 @@ def test_get_number_of_matching_pairs_of_socks():
     ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
     result = get_number_of_matching_pairs_of_socks(n, ar)
     assert result == 3
+
+
+def test_get_number_of_matching_pairs_of_socks_again():
+    n = 10
+    ar = [1, 1, 3, 1, 2, 1, 3, 3, 3, 3]
+    result = get_number_of_matching_pairs_of_socks(n, ar)
+    assert result == 4
