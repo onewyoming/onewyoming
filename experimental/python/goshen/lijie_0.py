a1c2bbfc9b5cbbfe03cc7fc1047ef0d8db46451e
Kushal <kushaldeveloper@gmail.com>
2019-04-09 07:53:06 -0400

    add binary search from stack overflow
    https://stackoverflow.com/a/34420349/227646

--- experimental/python/goshen/lijie_0.py
+++ experimental/python/goshen/lijie_0.py
@@ -1,5 +1,18 @@
 # give a sorted array find the last number n in the array and return its index
 
 
-def get_last_n(input_array, number_to_find):
-    return 8
+def get_last_n(input_array, value):
+    first = 0
+    last = len(input_array) - 1
+    found = False
+    while first <= last and not found:
+        midpoint = (first + last) // 2
+        if value == input_array[midpoint]:
+            found = True
+        else:
+            if value < input_array[midpoint]:
+                last = midpoint - 1
+            else:
+                if value > midpoint:
+                    first = midpoint + 1
+    return found
