9005a185b7a6e94e711dc21f365664ff7f2aa0cd
Kushal <kushaldeveloper@gmail.com>
2019-04-09 07:41:02 -0400

    add failing test

---
+++ experimental/python/goshen/lijie_0.py
@@ -0,0 +1,4 @@
+# give a sorted array find the last number n in the array and return its index
+
+def get_last_n(input_array, number_to_find):
+    return 8
---
+++ experimental/python/goshen/tests/test_lijie_0.py
@@ -0,0 +1,8 @@
+from lijie_0 import get_last_n
+
+
+def test_get_last_n():
+    input_array = [1, 2, 4, 6, 7, 7, 7, 7, 7, 9, 11, 40, 100]
+    n_to_find = 7
+    expected_index = 8
+    assert get_last_n(input_array, n_to_find) == expected_index
