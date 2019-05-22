056baaaeda0664ef90009246cd0ef4104f0814d8
Kushal <kushaldeveloper@gmail.com>
2019-05-22 19:39:12 -0400

    use a very stupid way to go through the whole array
    1. make it work
    2. make it bette
    3. ???
    4. profit

--- experimental/python/goshen/lijie_0.py
+++ experimental/python/goshen/lijie_0.py
@@ -2,17 +2,11 @@
 
 
 def get_last_n(input_array, value):
-    first = 0
-    last = len(input_array) - 1
-    found = False
-    while first <= last and not found:
-        midpoint = (first + last) // 2
-        if value == input_array[midpoint]:
-            found = True
-        else:
-            if value < input_array[midpoint]:
-                last = midpoint - 1
-            else:
-                if value > midpoint:
-                    first = midpoint + 1
-    return found
+    # loop through entire array 
+    # until we find something greater than value 
+    # or we reach end of array 
+    for index, element in enumerate(input_array):
+        if element > value:
+            return index - 1
+    return -1
+
