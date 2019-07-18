5526570bc3801cd2d573675f217d920f8ca16184
Kushal <kushaldeveloper@gmail.com>
2019-07-18 10:09:06 -0600

    add a new solution

---
+++ experimental/dotnet/hubcap/hubcap-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hubcap_api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
