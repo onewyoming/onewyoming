904b900dbc4ffe8d53b8f75b780ebab13457cc87
Kushal <kushaldeveloper@gmail.com>
2019-07-22 15:29:11 -0600

    add a new project called Noir
    because black is the new black

---
+++ experimental/dotnet/Noir/API/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id <= 0) 
+            {
+                return "You requested a non-positive id.";
+            } 
+            var stringBuilder = new System.Text.StringBuilder();
+            int parameter; 
+            if (id > 10)
+            {
+                parameter = 10;
+            }
+            else { parameter = id; }
+            for (int i = 0; i < parameter; i++)
+            {
+                stringBuilder.Append(i);
+            }
+            return stringBuilder.ToString();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
