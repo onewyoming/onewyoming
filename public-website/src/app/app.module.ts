6c0c7cc1e4421a8a337411b5bfb5110539eda3e3
Kushal <kushaldeveloper@gmail.com>
2019-07-27 10:10:45 -0600

    add a new component for navbar
    we will remove mat toolbar
    from app component

--- public-website/src/app/app.module.ts
+++ public-website/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
