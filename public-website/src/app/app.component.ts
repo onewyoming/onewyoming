f284feda9efdabc649aeefea567ab26d5bab5671
Kushal <kushaldeveloper@gmail.com>
2019-07-27 08:00:30 -0600

    format document using visual studio code

--- public-website/src/app/app.component.ts
+++ public-website/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
   title = 'Welcome to Wyoming!';
-  public constructor(private titleService: Title) {}
+  public constructor(private titleService: Title) { }
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
