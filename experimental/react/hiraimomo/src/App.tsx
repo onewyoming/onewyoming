c5195f05ff1a49818d762c1218c84fba6c1dad28
Kushal <kushaldeveloper@gmail.com>
2019-08-13 06:01:47 -0600

    change text
    there is something fundamentally wrong
    with putting html inside a javascript file.
    
    I don't like it and typescript can't fix that

--- experimental/react/hiraimomo/src/App.tsx
+++ experimental/react/hiraimomo/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello. <code>Hirai Momo</code> is a member of Twice.
         </p>
         <a
           className="App-link"
