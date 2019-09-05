e46b6850b7f2bee8bfa4fbdfce2851c881f94544
Kushal <kushaldeveloper@gmail.com>
2019-09-05 05:55:37 -0600

    add to create table sql script

--- experimental/sqlserver/Jeju/JejuScript/JejuScript/create_tables.sql
+++ experimental/sqlserver/Jeju/JejuScript/JejuScript/create_tables.sql
@@ -6,3 +6,15 @@ if exists (select * from sys.schemas where sys.schemas.name like 'jeju')
 		select 0;
 	end
 exec('create schema [Jeju]');
+if exists(select * from information_schema.tables where table_name like 'Person' and table_schema like 'jeju')
+	drop table [Jeju].[Person];
+create table [Jeju].[Person] 
+(
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[NAME] NVARCHAR(MAX) NOT NULL,
+	[DESCRIPTION] NVARCHAR(MAX) NOT NULL,
+	[LATITUDE] DECIMAL(9,6) NOT NULL,
+	[LONGITUDE] DECIMAL(9,6) NOT NULL,
+	[ROWVERSION] [timestamp] NOT NULL,
+	CONSTRAINT [PK_KUSHALNODE] PRIMARY KEY ([ID])
+);
