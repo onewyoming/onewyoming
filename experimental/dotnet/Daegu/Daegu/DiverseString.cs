748b3ec806056957a3fb5d28a270663b593b9e32
Kushal <kushaldeveloper@gmail.com>
2019-09-16 07:01:25 -0600

    remove console log and is almost complete boolean

--- experimental/dotnet/Daegu/Daegu/DiverseString.cs
+++ experimental/dotnet/Daegu/Daegu/DiverseString.cs
@@ -10,10 +10,7 @@ namespace Daegu
         {
             var result = "";
 
-            Console.WriteLine($"Input contains {A} As, {B} Bs, and {C} Cs.");
-
             var isComplete = false;
-            var isAlmostComplete = false;
             var iterator = 0;
 
             var inputDictionary = new Dictionary<string, int>();
@@ -27,9 +24,8 @@ namespace Daegu
             while (!isComplete)
             {
                 iterator++;
-                if(myList.First().Value == 0 || isAlmostComplete || myList[myList.Count - 1].Value <= 0 && myList[myList.Count - 2].Value <= 0) 
+                if(myList.First().Value == 0 || myList[myList.Count - 1].Value <= 0 && myList[myList.Count - 2].Value <= 0) 
                 {
-                    isAlmostComplete = true;
                     if(myList.First().Value == 0)
                     {
                         break;
@@ -43,7 +39,6 @@ namespace Daegu
                     var newEntry = new KeyValuePair<string, int>(myList[1].Key, oldValue - 1);
                     myList.Remove(myList[1]);
                     myList.Add(newEntry);
-                    Console.WriteLine($"The second key is {myList[1].Key.ToString()} and the value is {myList[1].Value.ToString()}");
                     myList = myList.OrderByDescending(element => element.Value).ToList();
                 }
                 else if (result.Length > 1 && result[result.Length - 1] == result[result.Length - 2] && myList[1].Value == 0)
@@ -52,18 +47,14 @@ namespace Daegu
                 }
                 else
                 {
-                    Console.WriteLine($"Entered the else. The first key is {myList.First().Key.ToString()} and the value is {myList.First().Value.ToString()}");
                     result += myList.First().Key.ToString();
-                    Console.WriteLine(result);
                     var oldValue = myList.First().Value;
                     var newEntry = new KeyValuePair<string, int>(myList.First().Key, oldValue - 1);
                     myList.Remove(myList.First());
                     myList.Add(newEntry);
-                    Console.WriteLine($"The first key is {myList.First().Key.ToString()} and the value is {myList.First().Value.ToString()}");
                     myList = myList.OrderByDescending(element => element.Value).ToList();
                 }
             }
-            Console.WriteLine($"The result is {result}");
             return result;
         }
     }
