b5ae6f0f34e1322636e29538f6df30ab41e3825e
Kushal <kushaldeveloper@gmail.com>
2019-09-21 12:22:13 -0600

    build and run daegu

---
+++ experimental/dotnet/Daegu/DaeguTests/MinimumCoinTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using Daegu;
+namespace DaeguTests
+{
+    public class MinimumCoinTests
+    {
+        [Fact]
+        public void Freebie()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
