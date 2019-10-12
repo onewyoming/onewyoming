3d186b03abc1ed2a21e8197144a0f21c510600ac
Kushal <kushaldeveloper@gmail.com>
2019-10-12 11:14:37 -0600

    add a freebie unit test

--- experimental/dotnet/wonju/WonjuBL/Class1.cs
+++ experimental/dotnet/wonju/WonjuBL/Class1.cs
@@ -4,5 +4,12 @@ namespace WonjuBL
 {
     public class Class1
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Class1(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
