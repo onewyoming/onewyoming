0d875eb907535df618755a90b5c379cd7b881b70
Kushal <kushaldeveloper@gmail.com>
2019-10-13 12:01:36 -0600

    drop and create table to fix spelling

--- experimental/dotnet/wonju/WonjuDB/WonjuDB/CreateTables.sql
+++ experimental/dotnet/wonju/WonjuDB/WonjuDB/CreateTables.sql
@@ -1,7 +1,8 @@
 use Wonju;
+drop table if exists Person;
 create table Person 
 (
 	ID int identity(1,1) primary key,
 	LEGAL_NAME nvarchar(max),
-	PREFFERRED_NAME nvarchar(max)
+	PREFERRED_NAME nvarchar(max)
 );
--- experimental/dotnet/wonju/WonjuDB/WonjuDB/InsertIntoTables.sql
+++ experimental/dotnet/wonju/WonjuDB/WonjuDB/InsertIntoTables.sql
@@ -1,3 +1,3 @@
 use Wonju;
-insert into Wonju.dbo.Person (LEGAL_NAME, PREFFERRED_NAME) values ('Kushal Hada', 'kus');
-select ID, LEGAL_NAME, PREFFERRED_NAME from Wonju.dbo.Person;
+insert into Wonju.dbo.Person (LEGAL_NAME, PREFERRED_NAME) values ('Kushal Hada', 'kus');
+select ID, LEGAL_NAME, PREFERRED_NAME from Wonju.dbo.Person;
