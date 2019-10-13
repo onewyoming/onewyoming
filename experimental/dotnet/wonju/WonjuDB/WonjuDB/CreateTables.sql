e76d690f7870c30757f8da397572467e0b8c87ff
Kushal <kushaldeveloper@gmail.com>
2019-10-13 11:59:41 -0600

    create table with typo

--- experimental/dotnet/wonju/WonjuDB/WonjuDB/CreateTables.sql
+++ experimental/dotnet/wonju/WonjuDB/WonjuDB/CreateTables.sql
@@ -1,2 +1,7 @@
 use Wonju;
 create table Person 
+(
+	ID int identity(1,1) primary key,
+	LEGAL_NAME nvarchar(max),
+	PREFFERRED_NAME nvarchar(max)
+);
---
+++ experimental/dotnet/wonju/WonjuDB/WonjuDB/InsertIntoTables.sql
@@ -0,0 +1,3 @@
+use Wonju;
+insert into Wonju.dbo.Person (LEGAL_NAME, PREFFERRED_NAME) values ('Kushal Hada', 'kus');
+select ID, LEGAL_NAME, PREFFERRED_NAME from Wonju.dbo.Person;
