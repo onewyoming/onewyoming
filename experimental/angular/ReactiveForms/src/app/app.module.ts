9a35bda34fec8e721f39db0cfc90556da0f95a30
Kushal <kushaldeveloper@gmail.com>
2019-10-19 20:47:19 -0600

    import reactive forms module

--- experimental/angular/ReactiveForms/src/app/app.module.ts
+++ experimental/angular/ReactiveForms/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { NameEditorComponent } from './name-editor/name-editor.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
