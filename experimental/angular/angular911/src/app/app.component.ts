aaca22404fbe0c073d015b200507d86564c6e51b
kushal <kushaldeveloper@gmail.com>
2020-04-14 15:55:47 -0600

    WIP subscribe

--- experimental/angular/angular911/src/app/app.component.ts
+++ experimental/angular/angular911/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from './item.service';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular911';
+  items: Array<Item>;
+  constructor(private itemService: ItemService) {
+    this.items = new Array<Item>();
+  }
+  ngOnInit() {
+    this.itemService.getAllItems().subscribe({
+      next: x => {
+        this.items = this.items.concat(x);
+        this.items.forEach(y => {
+          console.log(y.title);
+        });
+      },
+      error: x => {
+        console.error({ x });
+      },
+      complete: () => {
+        console.log('complete');
+      }
+    })
+  }
 }
