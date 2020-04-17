6bab1b5c34010ad30db23daa4bb6b62528a698d4
kushal <kushaldeveloper@gmail.com>
2020-04-17 17:09:38 -0600

    fix tests

--- experimental/angular/angular911/src/app/item.service.spec.ts
+++ experimental/angular/angular911/src/app/item.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ItemService } from './item.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ItemService', () => {
   let service: ItemService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ]
+    });
     service = TestBed.inject(ItemService);
   });
 
