6743d329694f8302b6fa24907779567f54dedf0a
kushal <kushaldeveloper@gmail.com>
2020-06-22 10:45:36 -0600

    do I need to add a null guard?
    doesn't look like I do

--- experimental/dotnet/Daegu/Daegu/Program.cs
+++ experimental/dotnet/Daegu/Daegu/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace Daegu
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Daegu
 {
+    class Potato
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            DiverseString diverseString = new DiverseString();
-            diverseString.GetString(0, 2, 8);
+            List<Potato> potatoes = new List<Potato>();
+            Potato potato1 = new Potato();
+            potato1.Id = 1;
+            potato1.Name = null;
+            potato1.Type = null;
+            potatoes.Add(potato1);
+            Potato potato2 = new Potato();
+            potato2.Id = 1;
+            potato2.Name = "Russett";
+            potato2.Type = "Idaho";
+            potatoes.Add(potato2);
+            var filteredPotatoes = potatoes.Where(potato => !(potato.Name == "Russett" && potato.Type == "Idaho")).ToList();
         }
     }
 }
