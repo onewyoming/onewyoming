af5736dcb9c8a9af47d1f88addf447bab50a0154
kushal <kushaldeveloper@gmail.com>
2020-06-25 22:07:58 -0600

    add a program
    doesn't work

--- experimental/dotnet/Daejeon/DapperConsole/Program.cs
+++ experimental/dotnet/Daejeon/DapperConsole/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Npgsql;
 
-namespace DapperConsole
+public class Program
 {
-    class Program
+    public class Person
     {
-        static void Main(string[] args)
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+
+
+    public static void Main()
+    {
+        string sqlPerson = "SELECT * FROM person;";
+
+
+        using(var connection = new NpgsqlConnection("postgres://hrzbdtnk:gFUPUNncgiHfgXa5C80TwV7nKYyU6tdQ@topsy.db.elephantsql.com:5432/hrzbdtnk"))
         {
-            Console.WriteLine("Hello World!");
+            var personDetails = connection.Query<Person>(sqlPerson).ToList();
+
+            Console.WriteLine(personDetails.Count);
         }
     }
+
 }
+
