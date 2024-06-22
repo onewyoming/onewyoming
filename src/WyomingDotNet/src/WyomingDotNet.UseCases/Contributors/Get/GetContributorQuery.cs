ae1e5aad09af0fef7ed364832da5cd7344085ede
Kushal <kushaldeveloper@gmail.com>
2024-06-22 08:26:37 -0400

    add a new project using clean architecture
    cd "C:\src\wyoming\src"; date; dotnet new install Ardalis.CleanArchitecture.Template; date;
    
    Saturday, June 22, 2024 8:20:12 AM
    The following template packages will be installed:
       Ardalis.CleanArchitecture.Template
    
    Success: Ardalis.CleanArchitecture.Template::9.1.2 installed the following templates:
    Template Name                        Short Name  Language  Tags
    -----------------------------------  ----------  --------  ------------------------------
    ASP.NET Clean Architecture Solution  clean-arch  [C#]      Web/ASP.NET/Clean Architecture
    
    Saturday, June 22, 2024 8:20:14 AM
    
    date; dotnet new clean-arch -o WyomingDotNet; date;
    
    Saturday, June 22, 2024 8:21:44 AM
    The template "ASP.NET Clean Architecture Solution" was created successfully.
    
    Saturday, June 22, 2024 8:21:44 AM

---
+++ src/WyomingDotNet/src/WyomingDotNet.UseCases/Contributors/Get/GetContributorQuery.cs
@@ -0,0 +1,6 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+
+namespace WyomingDotNet.UseCases.Contributors.Get;
+
+public record GetContributorQuery(int ContributorId) : IQuery<Result<ContributorDTO>>;
